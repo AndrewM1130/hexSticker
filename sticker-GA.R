800cc9434c985d51abc18a55602711cc64b5fdfe
Andrew Ma <andrewm2010796@gmail.com>
2022-01-11 20:43:22 -0800

    GA hexSticker created

---
+++ sticker-GA.R
@@ -0,0 +1,27 @@
+## required packages
+library(magick)
+library(dplyr)
+library(hexSticker)
+
+
+#basic image processing
+img <- image_read("figures/gene.jpeg")
+
+img %>%
+  image_convert("png") %>%
+  image_resize("1080 x 200")%>%
+  image_fill(color="#062047", point="+45") -> res
+
+res
+
+# wrap in plot to preview ie plot(sticker(...))
+final_res<-sticker(res, package="GA", p_size = 15,
+                   p_y = 1.5,
+                   s_x=1, s_y=0.8, s_width=1.1,
+                   s_height = 14,
+                   filename="stickerSample.png",
+                   h_fill="#062047",
+                   h_color = "#062047")
+
+plot(final_res)
+
